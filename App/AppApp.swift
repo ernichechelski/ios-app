202a372582734614d577bea2da4329d9b2082c1d
Ernest Chechelski <ernichechelski@gmail.com>
2022-06-06 14:31:26 +0200

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Ernest Chechelski on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
